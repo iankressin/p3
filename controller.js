ba352a17eb5bcf8450535cd46d9006ab26ec1456
Ian <ianguimaraes31@gmail.com>
2020-11-04 23:36:54 -0300

    Formatting outputs

--- controller.js
+++ controller.js
@@ -15,24 +15,60 @@ class Controller {
       J: 0.21782
     };
     this.percenteages = [0.3, 0.25, 0.2, 0.15, 0.1];
+    this.neighbors = new Neighbors(this.values, this.percenteages);
   }
 
-  itemA = () => {
-    const initialWallet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    let neighbors = new Neighbors(this.values, this.percenteages);
-    return neighbors.solution(initialWallet);
+  answer = () => {
+    this._itemA();
+    console.log("\n\n");
+    this._itemB();
   };
 
-  itemB = () => {};
+  _itemA = () => {
+    console.log("ITEM A: \n");
+    const initialWallet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    console.log(this.neighbors.solution(initialWallet));
+  };
 
-  itemC = () => {};
+  _itemB = () => {
+    console.log("ITEM B: \n");
+
+    const RANDOM_WALLETS = 10;
+    const wallets = [];
+
+    for (let i = 0; i < RANDOM_WALLETS; i++) {
+      const solution = this.neighbors.solution(this._getRandomWallet());
+      this._outputSolution(i + 1, solution);
+    }
+  };
 
   _getRandomWallet = () => {
-    for (let i = 0; i < this.values.length; i++) {}
+    let possibilities = Object.keys(this.values);
+    let counter = possibilities.length;
+
+    while (counter > 0) {
+      let index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      let temp = possibilities[counter];
+      possibilities[counter] = possibilities[index];
+      possibilities[index] = temp;
+    }
+
+    return possibilities;
   };
 
-  _randomPosition = () =>
-    Math.round(Math.random() * (Object.keys(this.values).length - 0)) + 0;
+  _outputSolution = (name, solution) => {
+    console.log(`Wallet ${name}:`);
+    console.log(`30%    --> ${solution.stocks[0]}`);
+    console.log(`25%    --> ${solution.stocks[1]}`);
+    console.log(`20%    --> ${solution.stocks[2]}`);
+    console.log(`15%    --> ${solution.stocks[3]}`);
+    console.log(`10%    --> ${solution.stocks[4]}`);
+    console.log(`PROFIT --> ${solution.profit}`);
+    console.log("\n");
+  };
 }
 
 module.exports = new Controller();
--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// TODO: Ao achar a melhor solução, continuar buscando até que não se ache mais nenhuma
-// solução melhor
-// TODO: Separar os itens da questão em A, B, C
 const controller = require("./controller");
 
-console.log(controller.itemA());
+controller.answer();
